909d0d0973d646257e0cc5736c1af4db2f9b8ebb
G303K <micder18@gmail.com>
2023-11-07 18:13:57 -0500

    Fixed error with server.js

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -1,30 +1,12 @@
-// Import Sequelize and create an instance of it
-const Sequelize = require('sequelize');
-const sequelize = new Sequelize('your_database', 'your_username', 'your_password', {
-  host: 'localhost',
-  dialect: 'mysql' // Change this to your database type
-});
-
-// Define your models
-const Product = sequelize.define('Product', {
-  // Define Product model attributes here
-});
-
-const Category = sequelize.define('Category', {
-  // Define Category model attributes here
-});
-
-const Tag = sequelize.define('Tag', {
-  // Define Tag model attributes here
-});
-
-const ProductTag = sequelize.define('ProductTag', {
-  // Define ProductTag model attributes here
-});
+const Product = require('./Product')
+const Category = require('./Category')
+const ProductTag = require('./ProductTag')
+const Tag = require('./Tag')
 
 // Define associations
 Product.belongsTo(Category);
 Category.hasMany(Product);
+
 
 Product.belongsToMany(Tag, { through: ProductTag });
 Tag.belongsToMany(Product, { through: ProductTag });
